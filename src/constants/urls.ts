08166e4e32dccff5e15430493a5854d36e511368
Pedro Agustin Vega <pedroagustinvega@proton.me>
2025-10-26 07:12:00 -0300

    New api endpoint

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,2 @@
-export const API_BASE: string = 'http://localhost:8001';
+export const API_BASE: string = 'https://api.pedroysofia.com';
 export const GITHUB_REPOSITORY: string = 'https://github.com/wrrulosdev/love-website';
